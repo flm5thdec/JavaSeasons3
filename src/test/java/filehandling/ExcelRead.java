8277f50858efb7a43f8fff86cae01c13522d1f71
seleniumriyaz <selenium.riyaz.shared@gmail.com>
2025-03-20 20:06:32 +0530

    Java Seasons 3 notes and programs till Mar 20th

--- src/test/java/filehandling/ExcelRead.java
+++ src/test/java/filehandling/ExcelRead.java
@@ -3,6 +3,9 @@ package filehandling;
 import java.io.FileInputStream;
 import java.io.IOException;
 
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ExcelRead {
@@ -13,7 +16,33 @@ public class ExcelRead {
 		
 		XSSFWorkbook wb=new XSSFWorkbook(fis1);
 		
+		XSSFSheet ws=wb.getSheet("Sheet1");
 		
+		int lastRowNumber=ws.getLastRowNum();
+		
+		System.out.println("getLastRowNum "+lastRowNumber);
+		
+		int noOfRows=ws.getPhysicalNumberOfRows();
+		
+		System.out.println("getPhysicalNumberOfRows  "+noOfRows);
+		
+		XSSFRow  row=ws.getRow(0);
+		
+		int lastCellNumber=row.getLastCellNum();
+		
+		System.out.println("lastCellNumber "+lastCellNumber);
+		
+		int physicalNumberOfCells= row.getPhysicalNumberOfCells();
+		
+		System.out.println("physicalNumberOfCells "+physicalNumberOfCells);
+		
+		XSSFCell  cell=row.getCell(0);
+		
+		System.out.println(cell);
+		
+		System.out.println(cell.getCellType());
+		
+		wb.close();
 
 	}
 
