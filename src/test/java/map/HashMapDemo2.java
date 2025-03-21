1484b7d8bc291019a6e8a879b6a1e1e21e5155fc
seleniumriyaz <selenium.riyaz.shared@gmail.com>
2025-03-21 20:17:43 +0530

    Java Seasons 3 notes and programs till Mar 21st

---
+++ src/test/java/map/HashMapDemo2.java
@@ -0,0 +1,31 @@
+package map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapDemo2 {
+
+	public static void main(String[] args) {
+		
+		
+		Map<String, String> hMap=new HashMap<String, String>();
+		
+		
+		System.out.println(hMap.size());  // 0
+		
+		hMap.put("Manager", "Ravi");
+		
+		hMap.put("Test Lead", "Gopi");
+		
+		hMap.put("Developer", "Kiran");
+		
+		hMap.put("Product Owner", "Mahesh");
+		
+		System.out.println(hMap.size());
+		
+		System.out.println(hMap);
+		 
+
+	}
+
+}
